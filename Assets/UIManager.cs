c9a34e08c3a0306c1b3ae24686148799e20744c2
Justin Murillo <G00302146@stu.raritanval.edu>
2022-12-06 19:46:52 -0500

    userinterface

---
+++ Assets/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public void StartGame() 
+{
+    SceneManager.LoadScene("RocketMouse");
+}
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void StartGame()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void
+    {
+        
+    }
+}
